ecabac9ace10021078097f5a3d034070ec1ebc88
Gemay1984 <gasv04@hotmail.com>
2026-04-15 11:43:55 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /**
  * ZIPCASTELLANO SERVICE WORKER
- * Versión: 3.6 (Update Force)
+ * Versión: 3.7 (Update Force)
  * Permite uso offline y mejora la velocidad de carga.
  */
 
-const CACHE_NAME = 'zipcastellano-v3.6';
+const CACHE_NAME = 'zipcastellano-v3.7';
 const CACHE_URLS = [
   './',
   './index.html',
